d8012558ba38ef1d517ff8bb89a06836f37ccb1a
xingwanxiang <xingwanxiang@autohome.com.cn>
2021-03-04 00:04:26 +0800

    rename BRCT domains

--- alignment1.js
+++ alignment1.js
@@ -1,43 +1,88 @@
 //seq-id	alignment-start	alignment-end
 const alignment = [
-    ['>evm.model.1.10740', 632, 681, 'PTCB-BRCT'],
-    ['>evm.model.1.10808', 265, 338, 'BRCT_2'],
-    ['>evm.model.1.3990', 47, 108, 'BRCT'],
-    ['>evm.model.1.7034', 150, 224, 'BRCT'],
-    ['>evm.model.2.613', 109, 180, 'BRCT'],
-    ['>evm.model.2.613', 917, 981, 'BRCT'],
-    ['>evm.model.3.1134', 595, 704, 'BRCT_2'],
-    ['>evm.model.3.1134', 485, 560, 'BRCT'],
+    ['evm.model.1.10740', 632, 681, 'PTCB-BRCT'],
+    ['evm.model.1.10808', 265, 338, 'BRCT_2'],
+    ['evm.model.1.3990', 47, 108, 'BRCT'],
+    ['evm.model.1.7034', 150, 224, 'BRCT'],
+    ['evm.model.2.613', 109, 180, 'BRCT'],
+    ['evm.model.2.613', 917, 981, 'BRCT'],
+    ['evm.model.3.1134', 595, 704, 'BRCT_2'],
+    ['evm.model.3.1134', 485, 560, 'BRCT'],
 
-    ['>evm.model.3.1204', 413, 482, 'BRCT'],
-    ['>evm.model.3.1486', 7, 66, 'BRCT'],
-    ['>evm.model.3.1486', 104, 181, 'BRCT'],
-    ['>evm.model.3.1541', 358, 419, 'BRCT'],
-    ['>evm.model.3.1648', 564, 641, 'BRCT'],
-    ['>evm.model.3.1840', 358, 419, 'PTCB-BRCT'],
-    ['>evm.model.3.2233', 667, 728, 'PTCB-BRCT'],
-    ['>evm.model.3.2233', 563, 646, 'BTCT_2'],
-    ['>evm.model.3.2233', 103, 172, 'BRCT'],
-    ['>evm.model.4.1000', 319, 389, 'BRCT'],
-    ['>evm.model.4.27', 78, 139, 'PTCB-BRCT'],
-    ['>evm.model.4.282', 78, 139, 'PTCB-BRCT'],
-    ['>evm.model.4.372', 661, 706, 'PTCB-BRCT'],
-    ['>evm.model.4.4602', 279, 350, 'BRCT'],
+    ['evm.model.3.1204', 413, 482, 'BRCT'],
+    ['evm.model.3.1486', 7, 66, 'BRCT'],
+    ['evm.model.3.1486', 104, 181, 'BRCT'],
+    ['evm.model.3.1541', 358, 419, 'BRCT'],
+    ['evm.model.3.1648', 564, 641, 'BRCT'],
+    ['evm.model.3.1840', 358, 419, 'PTCB-BRCT'],
+    ['evm.model.3.2233', 667, 728, 'PTCB-BRCT'],
+    ['evm.model.3.2233', 563, 646, 'BTCT_2'],
+    ['evm.model.3.2233', 103, 172, 'BRCT'],
+    ['evm.model.4.1000', 319, 389, 'BRCT'],
+    ['evm.model.4.27', 78, 139, 'PTCB-BRCT'],
+    ['evm.model.4.282', 78, 139, 'PTCB-BRCT'],
+    ['evm.model.4.372', 661, 706, 'PTCB-BRCT'],
+    ['evm.model.4.4602', 279, 350, 'BRCT'],
 
-    // ['>evm.model.6.575', 541, 667, 'BRCA-2_OB1'],
-    // ['>evm.model.6.575', 463, 537, 'BRCA-2_helical'],
-    // ['>evm.model.6.575', 92, 119, 'BRCA2'],
-    // ['>evm.model.6.575', 197, 224, 'BRCA2'],
-    ['>evm.model.8.2802', 968, 1064, 'BRCT_3'],
-    ['>evm.model.8.777', 9, 71, 'BRCT'],
-    // ['>evm.model.8.806', 9, 71, 'BRCT'],
+    // ['evm.model.6.575', 541, 667, 'BRCA-2_OB1'],
+    // ['evm.model.6.575', 463, 537, 'BRCA-2_helical'],
+    // ['evm.model.6.575', 92, 119, 'BRCA2'],
+    // ['evm.model.6.575', 197, 224, 'BRCA2'],
+    ['evm.model.8.2802', 968, 1064, 'BRCT_3'],
+    ['evm.model.8.777', 9, 71, 'BRCT'],
+    // ['evm.model.8.806', 9, 71, 'BRCT'],
 ]
 
+const names= {
+'evm.model.1.10740':'BRCT3',
+'evm.model.1.10808':'BRCT4',
+'evm.model.1.3990':'BRCT1',
+'evm.model.1.7034':'BRCT2',
+'evm.model.2.613':'BRCT5',
+'evm.model.3.1134':'BRCT6',
+'evm.model.3.1204':'BRCT7',
+'evm.model.3.1486':'BRCT8',
+'evm.model.3.1541':'BRCT9',
+'evm.model.3.1648':'BRCT10',
+'evm.model.3.1840':'BRCT11',
+'evm.model.3.2233':'BRCT12',
+'evm.model.4.1000':'BRCT17',
+'evm.model.4.27':'BRCT13',
+'evm.model.4.282':'BRCT15',
+'evm.model.4.372':'BRCT16',
+'evm.model.4.4602':'BRCT18',
+'evm.model.4.72':'BRCT14',
+'evm.model.6.575':'BRCT19',
+'evm.model.8.2802':'BRCT21',
+'evm.model.8.777':'BRCT20'
+}
+
+function brctNames(alignment) {
+    let namesWithNum = [];
+    let i = 1;
+    alignment.map((item, index) => {
+        let key = item[0] 
+        let suffix = ''
+        if(index<alignment.length-1 &&  key=== alignment[index+1][0]
+            || index>2 &&  key=== alignment[index-1][0]){
+            suffix =  "_" + (i++)
+
+        }
+        if(index<alignment.length-1 && key !== alignment[index+1][0]){ 
+            i = 1;
+        }
+        // debugger
+        
+        namesWithNum.push( names[key] + suffix)
+    })
+    return namesWithNum;
+}
 //output 'key', 'start', 'end', 'before', 'brct', 'after','name'
 //  where name is hmm-name, key is protein sequence id
 // brct is BRCT domain
 function calcTableData(alignment, brct, cutLength) {
     let data = [];
+    
     let i = 1;
     alignment.map((item, index) => {
         let key = item[0]
@@ -54,7 +99,16 @@ function calcTableData(alignment, brct, cutLength) {
         let brctCut = v.substr(start, end - start)
         let before = v.substr(start - cutLength, cutLength)
         let after = v.substr(end, cutLength)
-        data.push({ No: i++, key: key + "_" + (i - 1), start, end, before, 'brct': brctCut, after, name })
+        let suffix = ''
+        if(index<alignment.length-1 &&  key=== alignment[index+1][0]
+            || index>2 &&  key=== alignment[index-1][0]){
+            suffix =  "_" + (i++)
+        }
+        if(index<alignment.length-1 && key !== alignment[index+1][0]){ 
+            i = 1;
+        }
+        
+        data.push({ No: index+1, key: names[key] + suffix, start, end, before, 'brct': brctCut, after, name })
     })
     return data;
 }
